da52ebb1fab456064e873e3a7966828ff62a0890
Abhishek kumar Prajapati <43763106+akpiitr@users.noreply.github.com>
2025-02-17 22:29:31 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ document.querySelectorAll('.navbar a').forEach(anchor => {
         });
     });
 });
+
+document.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("shrink");
+    } else {
+        navbar.classList.remove("shrink");
+    }
+});
